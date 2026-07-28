fd3ba452305f0292b1d34f2cccddaec8e9f433f8
thriveni2945 <purinithriveni@gmail.com>
2026-07-29 05:09:45 +0530

    Time: 3 ms (64.36%), Space: 19.3 MB (63.12%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = list(map(str, nums))
+
+        def compare(a, b):
+            if a + b > b + a:
+                return -1
+            else:
+                return 1
+
+        from functools import cmp_to_key
+        nums.sort(key=cmp_to_key(compare))
+
+        if nums[0] == "0":
+            return "0"
+
+        return ''.join(nums)
+
+        
