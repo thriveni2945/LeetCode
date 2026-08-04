2d1bfc6e0fe637e4dda89b9ac172009c9502f963
thriveni2945 <purinithriveni@gmail.com>
2026-08-04 18:16:44 +0530

    Time: 735 ms (86.14%), Space: 0B (100%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+DELETE FROM Person
+WHERE Id NOT IN (
+    SELECT * FROM (
+        SELECT MIN(Id)
+        FROM Person
+        GROUP BY Email
+    ) AS temp
+);
