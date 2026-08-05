1884d9188a2225b383adb945be619a6895a878dc
thriveni2945 <purinithriveni@gmail.com>
2026-08-05 16:08:23 +0530

    Time: 0 ms (100%), Space: 19.5 MB (22.68%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -0,0 +1,20 @@
+class Solution:
+    def binaryTreePaths(self, root):
+        ans = []
+
+        def dfs(node, path):
+            if not node:
+                return
+
+            path += str(node.val)
+
+            if not node.left and not node.right:
+                ans.append(path)
+                return
+
+            path += "->"
+            dfs(node.left, path)
+            dfs(node.right, path)
+
+        dfs(root, "")
+        return ans
