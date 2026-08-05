bd6277537ce584ddf6df2ce68d922bb5ed6bef8f
thriveni2945 <purinithriveni@gmail.com>
2026-08-05 16:10:16 +0530

    Time: 723 ms (15.19%), Space: 0B (100%) - LeetHub

---
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -0,0 +1,19 @@
+SELECT
+    Request_at AS Day,
+    ROUND(
+        SUM(Status != 'completed') / COUNT(*),
+        2
+    ) AS "Cancellation Rate"
+FROM Trips
+WHERE Client_Id NOT IN (
+    SELECT Users_Id
+    FROM Users
+    WHERE Banned = 'Yes'
+)
+AND Driver_Id NOT IN (
+    SELECT Users_Id
+    FROM Users
+    WHERE Banned = 'Yes'
+)
+AND Request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY Request_at;
