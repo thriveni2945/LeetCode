a3d671f19f1b0e89b95d85b40303f5debd09b418
thriveni2945 <purinithriveni@gmail.com>
2026-08-05 17:00:11 +0530

    Time: 48 ms (90.44%), Space: 21 MB (25.43%) - LeetHub

---
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+class Solution:
+    def levelOrder(self, root):
+        if not root:
+            return []
+
+        ans = []
+        q = deque([root])
+
+        while q:
+            level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                level.append(node.val)
+                for child in node.children:
+                    q.append(child)
+            ans.append(level)
+
+        return ans
