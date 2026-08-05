6f5c1d835fed55a1bdc0325bfb15d3cf650a2157
thriveni2945 <purinithriveni@gmail.com>
2026-08-05 17:11:01 +0530

    Time: 0 ms (100%), Space: 21.9 MB (96.74%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -0,0 +1,23 @@
+class Solution:
+    def deleteNode(self, root, key):
+        if not root:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+
+            temp = root.right
+            while temp.left:
+                temp = temp.left
+
+            root.val = temp.val
+            root.right = self.deleteNode(root.right, temp.val)
+
+        return root
