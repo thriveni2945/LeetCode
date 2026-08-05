6c4e5216e1d1dbc45a3a706964a16b05b54ee5b2
thriveni2945 <purinithriveni@gmail.com>
2026-08-05 16:13:12 +0530

    Time: 0 ms (100%), Space: 24.5 MB (64.67%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n=len(citations)
+        l,r=0,n-1
+        while l<=r:
+            m=(l+r)//2
+            if citations[m]>=n-m:
+                r=m-1
+            else:
+                l=m+1
+        return n-l
+
+        
